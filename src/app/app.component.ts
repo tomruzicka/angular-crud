3bb0dde57ba690db5d289105135b6c334a80fe18
Tomas Ruzicka <mail.ruzicka@gmail.com>
2022-05-04 14:23:33 +0200

    angular-crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,113 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-crud';
+export class AppComponent implements OnInit {
+  title = 'angularCRUD';
+
+  displayedColumns: string[] = [
+    'productName',
+    'category',
+    'date',
+    'freshness',
+    'price',
+    'comment',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private dialog: MatDialog,
+    private api: ApiService,
+    private toast: NgToastService
+  ) {}
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+  openDialog() {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getAllProducts();
+        }
+      });
+  }
+
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        // console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        this.toast.error({
+          detail: 'ERROR',
+          summary: 'Data fetch failed',
+          duration: 5000,
+        });
+      },
+    });
+  }
+
+  editProduct(row: any) {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+        data: row,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getAllProducts();
+        }
+      });
+  }
+
+  deleteProduct(id: number) {
+    this.api.deleteProduct(id).subscribe({
+      next: (res) => {
+        this.toast.success({
+          detail: 'SUCCESS',
+          summary: 'Deleting is Success',
+          duration: 5000,
+        });
+        this.getAllProducts();
+      },
+      error: () => {
+        this.toast.error({
+          detail: 'ERROR',
+          summary: 'Delete failed',
+          duration: 5000,
+        });
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  // Post
+  postProduct(data: any) {
+    return this.http.post<any>('http://localhost:3000/productList/', data);
+  }
+
+  // Get
+  getProduct() {
+    return this.http.get<any>('http://localhost:3000/productList/');
+  }
+
+  // Update
+  putProduct(data: any, id: number) {
+    return this.http.put<any>('http://localhost:3000/productList/' + id, data);
+  }
+
+  // Delete
+  deleteProduct(id: number) {
+    return this.http.delete<any>('http://localhost:3000/productList/' + id);
+  }
+}
